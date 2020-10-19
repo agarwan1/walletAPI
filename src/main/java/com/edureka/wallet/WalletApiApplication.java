cc8da9964e06f215ac44d22d378c0cd9e74bd1c3
agarwan1 <agarwan1192@gmail.com>
2020-10-19 14:27:00 -0400

    Adding logging

--- src/main/java/com/edureka/wallet/WalletApiApplication.java
+++ src/main/java/com/edureka/wallet/WalletApiApplication.java
@@ -2,11 +2,17 @@ package com.edureka.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class WalletApiApplication {
-
+	static final Logger log = 
+	        LoggerFactory.getLogger(WalletApiApplication.class);
 	public static void main(String[] args) {
+		log.info("Before Starting application");
 		SpringApplication.run(WalletApiApplication.class, args);
+		log.debug("Starting my application in debug with {} args", args.length);
+		log.info("Starting my application with {} args.", args.length);  
 	}
 }
